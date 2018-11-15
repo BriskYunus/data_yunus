b79068e88e8fdcd3590567efa9857f3b826a7431
Yunus <b_yunus@tss-hr420-dm35.fc.ca>
2018-11-15 14:53:33 -0500

    added data

---
+++ pythonwithCSV.py
@@ -0,0 +1,71 @@
+import csv
+import numpy as np
+import matplotlib.pyplot as plt
+
+# figure out what data we want to use
+categories = [] # these are the headers in the CSV file
+installs = [] # this is the install row
+ratings = [] # this is the ratings row
+
+with open('data/googeplaystore.csv') as csvfile:
+	reader = csv.reader(csvfile)
+	line_count = 0
+
+	for row in reader:
+		# move the page column headers out of the actual data to get a clean dataset
+		
+		if line_count is 0: # this iwll be text, not data
+			print('pushing categories into a separate array')
+			categories.append(row) # push the text into this array
+			line_count += 1 # increment the line count for the next loop
+		else:
+			# grab the ratings and push them into the rating array
+			ratingsData = row[2]
+			ratingsData = ratingsData.replace("NaN", "0")
+			ratings.append(float(ratingsData)) # int will turn string of (piece of text) into a number
+			# print ('pushing ratings data into the ratings array')
+			installData = row[5]
+			installData = installData.replace(",", "") # get rid of commas
+
+			installs.append(np.char.strip(installData, "+")) # get rid of the trailing "+"
+			line_count += 1
+
+# get some values we can work with
+# how many ratings are 4+?
+# how many are below 2?
+# how many are in the middle?
+np_ratings = np.array(ratings)	# turn a plain Python list into a Numpy array
+popular_apps = np_ratings > 4
+print("popular apps:", len(np_ratings[popular_apps]))
+
+percent_popular = int(len(np_ratings[popular_apps]) / len(np_ratings) * 100)
+print(percent_popular)
+
+unpopular_apps = np_ratings < 4
+print("popular apps:", len(np_ratings[unpopular_apps]))
+
+percent_unpopular = int(len(np_ratings[unpopular_apps]) / len(np_ratings) * 100)
+print(percent_unpopular)
+
+kinda_popular = 100 - (percent_popular + percent_unpopular)
+print(kinda_popular)
+
+# do a visualization with our shiny new data
+lables = "Sucks", "Meh", "Love it!"
+sizes = [percent_unpopular, kinda_popular, percent_popular]
+colors = ['yellowgreen', 'lightgreen', 'lightskyblue']
+explode = (0.1, 0.1, 0.15)
+
+plt.pie(sizes, explode=explode, colors=colors, autopct='%1.1f%%', shadow=True, startangle=140)
+
+plt.axis('equal')
+plt.legend(lables, loc=1)
+plt.title("Do we love us some apps?")
+plt.xlabel("User Ratings - App Installs (10,000+ apps")
+plt.show()
+
+
+#	print('processed', line_count, 'lines of data')
+print(categories)
+print('first row of data:', installs[0])
+print('last row of data:', installs[-1])
